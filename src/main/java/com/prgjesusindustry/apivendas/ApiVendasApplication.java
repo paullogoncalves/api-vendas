535b1b39a0823b9d05be0216609e612ac5686108
Paulo Goncalves <paullogoncalves@gmail.com>
2021-04-12 15:48:00 -0300

    Categoria pagination

--- src/main/java/com/prgjesusindustry/apivendas/ApiVendasApplication.java
+++ src/main/java/com/prgjesusindustry/apivendas/ApiVendasApplication.java
@@ -71,6 +71,11 @@ public class ApiVendasApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -84,7 +89,7 @@ public class ApiVendasApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		
-		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
