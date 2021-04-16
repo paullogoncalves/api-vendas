0583357cd5accf5d8c95617dbd8f813ea5652fe2
Paulo Goncalves <paullogoncalves@gmail.com>
2021-04-16 15:05:50 -0300

    Validação customizada: email não repetido

--- src/main/java/com/prgjesusindustry/apivendas/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/prgjesusindustry/apivendas/services/validation/ClienteInsertValidator.java
@@ -6,11 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.prgjesusindustry.apivendas.domain.Cliente;
 import com.prgjesusindustry.apivendas.domain.enums.TipoCliente;
 import com.prgjesusindustry.apivendas.dto.ClienteNewDTO;
+import com.prgjesusindustry.apivendas.repositories.ClienteRepository;
 import com.prgjesusindustry.apivendas.resources.exception.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
