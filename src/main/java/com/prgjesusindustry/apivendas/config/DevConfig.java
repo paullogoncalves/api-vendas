df9489ad2a2efcce116553505fcb33bba0cce762
Paulo Goncalves <paullogoncalves@gmail.com>
2021-04-24 10:48:55 -0300

    Alterando profile de desenvolvimento

---
+++ src/main/java/com/prgjesusindustry/apivendas/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.prgjesusindustry.apivendas.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.prgjesusindustry.apivendas.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDataBase() {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	
+	
+}
